472b02cc4acda83b3a94d3027f94212903541edf
Timothee Simon <tim@mirahi.io>
2022-05-29 09:37:21 +0200

    improved presence detection and added gaby

--- apps/home/src/automations/personDetection.ts
+++ apps/home/src/automations/personDetection.ts
@@ -1,12 +1,18 @@
 import mqtt from 'mqtt'
-import { effect } from "@herja/core";
+import { effect, shadowState } from "@herja/core";
 import { sensor } from "generated/src";
 
 export const personDetection = (client:mqtt.MqttClient) => {
   effect(()=>{
-    if(sensor.tims_ipone_ssid.state === "Private" || sensor.tim_s_phone_room_presence.state === "home")
+    if(sensor.tims_ipone_ssid.state === "Private" || shadowState["device_tracker.tim_s_phone_tracker"] === "home" || shadowState["device_tracker.tim_iphone_ip"].state === "home" )
       client.publish('herja/sensor/tim_presence', "home");
     else
       client.publish('herja/sensor/tim_presence', "not_home");
-  }, [sensor.tims_ipone_ssid, sensor.tim_s_phone_room_presence])
+  }, [sensor.tims_ipone_ssid, "device_tracker.tim_s_phone_tracker", "device_tracker.tim_iphone_ip"])
+  effect(()=>{
+    if(shadowState["device_tracker.gaby_s_phone_tracker"] === "home" || shadowState["device_tracker.kapy"].state === "home" )
+      client.publish('herja/sensor/gaby_presence', "home");
+    else
+      client.publish('herja/sensor/gaby_presence', "not_home");
+  }, ["device_tracker.gaby_s_phone_tracker", "device_tracker.kapy"])
 };
