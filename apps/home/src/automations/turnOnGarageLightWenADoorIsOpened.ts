413b7c3c97041a6ef69203f75e90455e53dd7ea1
Tim <tim@xorob.io>
2022-12-12 21:57:29 +0100

    Better detection in garage

--- apps/home/src/automations/turnOnGarageLightWenADoorIsOpened.ts
+++ apps/home/src/automations/turnOnGarageLightWenADoorIsOpened.ts
@@ -5,7 +5,7 @@ let timeoutID: NodeJS.Timer|undefined = undefined;
 
 export const turnOnGarageLightWhenADoorIsOpened = () => {effect(() => {
   if(binary_sensor.garage_electric_door_sensor_closed_contact.isOn() ||
-    binary_sensor.garage_door_contact.isOn()
+    binary_sensor.garage_door_contact.isOn() || binary_sensor.g4_bullet_motion.isOn()
   ) {
     clearTimeout(timeoutID as number|undefined);
     light.garage.turnOn()
