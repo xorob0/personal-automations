f0b0c854f7d54bc7e8667530bd7e869134a4737d
Tim <tim@xorob.io>
2023-04-10 23:15:12 +0200

    wip

--- apps/home/src/automations/turnOnSomeLightsWhenComingHome.ts
+++
@@ -1,3 +0,0 @@
-export const turnOnSomeLightsWhenComingHome = () => {
-  // TODO Delete me
-};
--- apps/home/src/index.ts
+++ apps/home/src/index.ts
@@ -2,17 +2,16 @@ import { configure } from "@herja/core";
 import { garage } from "@herja/automations";
 import { turnOnGardenLightWithGardenDoorAtNight } from "./automations/turnOnGardenLightWithGardenDoorAtNight";
 import { turnOnGarageLightWhenADoorIsOpened } from "./automations/turnOnGarageLightWenADoorIsOpened";
-import { nightMode } from "./automations/nightMode";
+// import { nightMode } from "./automations/nightMode";
 import { wallSwitches } from "./automations/wallSwitches";
 import { automaticAlarm } from "./automations/automaticAlarm";
 import { tripodButton } from "./automations/tripodButton";
-import { coffeeMachineButton } from "./automations/coffeeMachineButton";
+// import { coffeeMachineButton } from "./automations/coffeeMachineButton";
 import { garageRemote } from "./automations/garageRemote";
 import { turnOnTripodOnSunset } from "./automations/turnOnTripodOnSunset";
 import { turnOnToiletLightWithDoor } from "./automations/TurnOnToiletLightWithDoor";
-// import { TurnOnUpstairsHallWithSensor } from "./automations/TurnOnUpstairsHallWithSensor";
+import { TurnOnUpstairsHallWithSensor } from "./automations/TurnOnUpstairsHallWithSensor";
 import { BedroomACTimer } from "./automations/bedroomACTimer";
-// import { turnOnSomeLightsWhenComingHome } from "./automations/turnOnSomeLightsWhenComingHome";
 import { turnEverythingOffWhenLeaving } from "./automations/turnEverythingOffWhenLeaving";
 import { personDetection } from "./automations/personDetection";
 import { deskButton } from "./automations/DeskButton";
@@ -22,7 +21,8 @@ import { roomBusy } from "./automations/roomBusy";
 import { createHumidifiers } from "./automations/humidifiers";
 import { hvacOnHumidity } from "./automations/hvacOnHumidity";
 import { machines } from "./automations/machines";
-// import { livingRoomRemote } from "./automations/LivingRoomRemote";
+import { livingRoomRemote } from "./automations/LivingRoomRemote";
+import { hallPresenceSensor } from "./automations/HallPresenceSensor";
 
 require('dotenv').config();
 
@@ -37,17 +37,16 @@ const base = async () => {
   turnOnGarageLightWhenADoorIsOpened();
   turnOnGardenLightWithGardenDoorAtNight();
   turnEverythingOffWhenLeaving();
-  // turnOnSomeLightsWhenComingHome()
-  nightMode()
+  // nightMode()
   wallSwitches()
   automaticAlarm()
   tripodButton()
-  coffeeMachineButton()
+  // coffeeMachineButton()
   allAlertsOpenedTooLong()
   garageRemote()
   turnOnTripodOnSunset()
   turnOnToiletLightWithDoor()
-  // TurnOnUpstairsHallWithSensor()
+  TurnOnUpstairsHallWithSensor()
   personDetection()
   garage();
   BedroomACTimer();
@@ -57,8 +56,8 @@ const base = async () => {
   createHumidifiers()
   hvacOnHumidity()
   machines()
-  // livingRoomRemote()
-  // hallPresenceSensor()
+  livingRoomRemote()
+  hallPresenceSensor()
 };
 
 base().catch(e => console.error("[index] error: ", e));
